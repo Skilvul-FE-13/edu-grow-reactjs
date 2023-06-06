c14965b5f85f533936915939a5ddeedc700cf3d8
maya aulia <mayaaulia601@gmail.com>
2023-06-06 11:59:23 +0700

    add static card and title

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import About from "../pages/About/About";
-// import KelasPage from "../pages/KelasPage/KelasPage";
 import GrowProgram from "../pages/GrowProgram/GrowProgram";
 
 export default function Router() {
@@ -10,8 +9,6 @@ export default function Router() {
             <Route path="/" element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="growProgram" element={<GrowProgram/>} />
-
-            {/* <Route path="kelas" element={<KelasPage />} /> */}
         </Routes>
     );
 }
--- src/pages/GrowProgram/GrowProgram.jsx
+++ src/pages/GrowProgram/GrowProgram.jsx
@@ -1,5 +1,9 @@
 import { Fragment } from "react";
 import { Helmet } from "react-helmet";
+import PartnerSection from "../../components/Layout/PartnerSection/PartnerSection";
+import CardComponent from "../../components/Layout/CardComponent/CardComponent";
+import { Container } from "../../components/core/Grid";
+import "./style.css"
 
 const titleTag = "EduGrow.id";
 const metaDesc = "Majukan Bangsa";
@@ -12,7 +16,15 @@ export default function GrowProgram() {
                 <meta name="title" content={titleTag} />
                 <meta name="description" content={metaDesc} />
             </Helmet>
-
+            <Container>
+                <section className="mx-0 my-8 grid gap-2">
+                <p className="font-color font-semibold">#BelajarDariAhlinya</p>
+                <h1 className="text-4xl font-extrabold">Kelas Grow Program</h1>
+                <p className="text-slate-500">Belajar mendasain tampilan website atau mobile apps dan juga berkolaborasi dengan tim developer</p>
+                </section>
+            </Container>
+            <CardComponent/>
+            <PartnerSection variant={"secondary"} />
             {/* Export Bagian Lain, contoh di Home.jsx */}
         </Fragment>
     );
