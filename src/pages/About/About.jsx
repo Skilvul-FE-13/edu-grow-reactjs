2a9748b191c1c2ff3da7ead855d337e4717ddb2f
Nandazzz <123712584+Nandazzz@users.noreply.github.com>
2023-06-06 08:21:07 +0700

    modif section reason

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import { Helmet } from "react-helmet";
 import AboutCTA from "./AboutCTA";
 import PartnerSection from "../../components/Layout/PartnerSection/PartnerSection";
+import AboutReason from "./AboutReason";
 
 const titleTag = "About | EduGrow.id";
 const metaDesc = "Majukan Bangsa";
@@ -17,6 +18,7 @@ export default function About() {
 
             <AboutCTA />
             <PartnerSection variant={"primary"} />
+            <AboutReason />
         </Fragment>
     );
 }
--- src/pages/About/AboutReason.jsx
+++ src/pages/About/AboutReason.jsx
@@ -1,34 +1,42 @@
 import { Button } from "flowbite-react";
 import "./styleAbout.css";
 
-export default function AboutCTA() {
+export default function AboutReason() {
     return (
         <section className="dark:bg-gray-900 space-y-0 bg-white">
             <div className="mx-auto grid max-w-screen-xl px-4 py-8 lg:grid-cols-12 lg:gap-8 lg:py-16 xl:gap-0 ">
-                <div className="mr-auto place-self-center lg:col-span-7">
-                    <h1 className="txt-scnd mb-4 max-w-2xl text-6xl font-extrabold leading-none tracking-tight">
-                        #SelangkahdiDepan
+                <div className="mr-auto lg:col-span-7">
+                    <h1 className="txt-prmy-color mb-4 max-w-2xl text-5xl font-extrabold leading-none tracking-tight">
+                        Kenapa Memilih Kami ?
                     </h1>
-                    <div className="flex items-center space-x-4">
-                        <span className="txt-scnd mb-4 max-w-2xl text-5xl font-extrabold leading-none tracking-tight">
-                            Bersama
-                        </span>
-                        <span className="txt-prmy-color mb-4 max-w-2xl text-6xl font-extrabold leading-none tracking-tight">
-                            EduGrow
-                        </span>
-                    </div>
+                    <br></br>
+                    <h2 className="txt-prmy-color mb-4 max-w-2xl text-4xl font-semibold leading-none tracking-tight">
+                        01. Mudah
+                    </h2>
                     <p className="dark:text-gray-400 mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
-                        Mulai belajar anti ribet. Berbagai macam jenis program
-                        pembelajaran dengan materi-materi terupdate serta
-                        berbagai macam Artikel menarik. Grow sekarang bersama
-                        Edu Grow GRATIS!
+                        EduGrow memberikan kamu kemudahan belajar dari para
+                        ahlinya dimanapun dan kapanpun kamu mau.
                     </p>
-                    <Button className="btn font-semibold" size={"xl"}>
-                        Grow Program
-                    </Button>
+                    <br></br>
+                    <h2 className="txt-prmy-color mb-4 max-w-2xl text-4xl font-semibold leading-none tracking-tight">
+                        02. Sertifikat
+                    </h2>
+                    <p className="dark:text-gray-400 mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
+                        Setelah menyelesaikian pembelajaran kamu akan
+                        mendapatkan sertifikat sebgai bukti kualifikasi kamu
+                        yang akan berguna kedepannya.
+                    </p>
+                    <br></br>
+                    <h2 className="txt-prmy-color mb-4 max-w-2xl text-4xl font-semibold leading-none tracking-tight">
+                        03. Experience
+                    </h2>
+                    <p className="dark:text-gray-400 mb-6 max-w-2xl font-light text-gray-500 md:text-lg lg:mb-8 lg:text-xl">
+                        Kamu akan mendapat banyak pengalaman untuk mengasan hard
+                        sikll maupun soft skill untuk menambah kemampuan kamu.
+                    </p>
                 </div>
                 <div className="hidden lg:col-span-5 lg:mt-0 lg:flex ">
-                    <img src="/images/About-heroCTA.png" alt="mockup" />
+                    <img src="/images/Section-reason.png" alt="mockup" />
                 </div>
             </div>
         </section>
