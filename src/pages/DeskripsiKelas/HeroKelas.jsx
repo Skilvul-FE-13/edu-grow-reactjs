36ce13d97fa5b5f09318b3c47cbac672fd289a48
Daniel Ryan Simatupang <danielryan240202@gmail.com>
2023-06-11 19:58:42 +0700

    hero progress

--- src/pages/DeskripsiKelas/HeroKelas.jsx
+++ src/pages/DeskripsiKelas/HeroKelas.jsx
@@ -3,11 +3,8 @@ import "./styleKelas.css";
 export default function HeroKelas() {
     return (
         <section>
-            <div className="px-40 py-40">
-                <div className="">
-                    <span>UI/UX Design Website Design With Figma</span>
-                </div>
-            </div>
+            <p>hello</p>
+            <img className="" src="/images/hero-img.jpg" alt="hero" />
         </section>
     );
 }
