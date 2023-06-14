ead5620446553551e35cc0a1798927a54615023a
Daniel Ryan Simatupang <danielryan240202@gmail.com>
2023-06-14 20:12:12 +0700

    responsive refisi

--- src/pages/DeskripsiKelas/DeskripsiKelasCTA.jsx
+++ src/pages/DeskripsiKelas/DeskripsiKelasCTA.jsx
@@ -1,56 +1,59 @@
 import { Button } from "flowbite-react";
 import "./styleDescKelas.css";
+import { Container } from "../../components/core/Grid";
 
 export default function DeskripsiKelasCTA() {
     return (
         <section className="bg-[url('/images/hero-img.jpg')] bg-cover bg-no-repeat">
-            <div className=" flex flex-col gap-6 pb-40 pl-80 pt-40">
-                <h1 className="text-justify text-4xl font-bold text-white">
-                    UI/UX Design Website With Figma
-                </h1>
-                <p className="text-justify text-base text-white">
-                    Dari yang awam bisa jadi ahli buat bikin produk digital
-                    keren
-                </p>
-                <div className="flex flex-row gap-8 text-white">
-                    <div className="flex flex-row items-center gap-3 ">
-                        <img src="/images/course.png" />
-                        <p>3 Courses</p>
-                    </div>
-                    <div className="flex flex-row items-center gap-3 ">
-                        <img src="/images/duration.png" />
-                        <p>3h 18m</p>
-                    </div>
-                    <div className="flex flex-row items-center gap-3 ">
-                        <img src="/images/video.png" />
-                        <p>18 video</p>
-                    </div>
+            <Container>
+                <div className=" flex flex-col gap-6 pb-40 pt-40">
+                    <h1 className="text-justify text-4xl font-bold text-white">
+                        UI/UX Design Website With Figma
+                    </h1>
+                    <p className="text-justify text-base text-white">
+                        Dari yang awam bisa jadi ahli buat bikin produk digital
+                        keren
+                    </p>
+                    <div className="flex flex-row gap-8 text-white">
+                        <div className="flex flex-row items-center gap-3 ">
+                            <img src="/images/course.png" />
+                            <p>3 Courses</p>
+                        </div>
+                        <div className="flex flex-row items-center gap-3 ">
+                            <img src="/images/duration.png" />
+                            <p>3h 18m</p>
+                        </div>
+                        <div className="flex flex-row items-center gap-3 ">
+                            <img src="/images/video.png" />
+                            <p>18 video</p>
+                        </div>
 
-                    <div className="flex flex-row items-center gap-3 ">
-                        <img src="/images/read.png" />
-                        <p>18 Reading Mterial</p>
+                        <div className="flex flex-row items-center gap-3 ">
+                            <img src="/images/read.png" />
+                            <p>18 Reading Mterial</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-row gap-8 text-white">
+                        <div className="flex flex-row items-center gap-3 ">
+                            <img src="/images/task.png" />
+                            <p>2 Kuis dan Latihan</p>
+                        </div>
+                        <div className="flex flex-row items-center gap-3 ">
+                            <img src="/images/sertif.png" />
+                            <p>Sertifikat</p>
+                        </div>
+                    </div>
+                    <p className="text-justify text-base text-white">
+                        Gabung kelasnya dan nikmati serunya pengalaman belajar
+                        di EduGrow
+                    </p>
+                    <div className="button">
+                        <Button className="btn-join font-semibold" size={"xl"}>
+                            Gabung Sekarang
+                        </Button>
                     </div>
                 </div>
-                <div className="flex flex-row gap-8 text-white">
-                    <div className="flex flex-row items-center gap-3 ">
-                        <img src="/images/task.png" />
-                        <p>2 Kuis dan Latihan</p>
-                    </div>
-                    <div className="flex flex-row items-center gap-3 ">
-                        <img src="/images/sertif.png" />
-                        <p>Sertifikat</p>
-                    </div>
-                </div>
-                <p className="text-justify text-base text-white">
-                    Gabung kelasnya dan nikmati serunya pengalaman belajar di
-                    EduGrow
-                </p>
-                <div className="button">
-                    <Button className="btn-join font-semibold" size={"xl"}>
-                        Gabung Sekarang
-                    </Button>
-                </div>
-            </div>
+            </Container>
         </section>
     );
 }
