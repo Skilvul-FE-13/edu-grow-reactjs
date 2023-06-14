bc864cf1326f7ce8a3712b59f92ee4d6fb159b21
maya aulia <mayaaulia601@gmail.com>
2023-06-15 00:21:32 +0700

    make content dynamic and update responsive

--- src/pages/DeskripsiKelas/Question.jsx
+++ src/pages/DeskripsiKelas/Question.jsx
@@ -1,44 +1,119 @@
 import "./styleDescKelas.css";
-import { Container } from "../../components/core/Grid";
 
 export default function Question() {
     return (
-        <Container>
-            <section className="pb-32 pt-16">
-                <div className="text-center">
-                    <h3 className="text-base font-medium text-blue-900">
-                        Mari Bertanya!
-                    </h3>
-                    <h1 className="text-3xl font-bold">Frequently Asked</h1>
-                    <h1 className="text-3xl font-bold">Question</h1>
+        <section className="pb-32 pt-16">
+            <div className="text-center">
+                <h3 className="text-base font-medium text-blue-900">
+                    Mari Bertanya!
+                </h3>
+                <h1 className="text-3xl font-bold">Frequently Asked</h1>
+                <h1 className="text-3xl font-bold">Question</h1>
+            </div>
+            <div className="grid grid-cols-2 gap-4 pt-12 max-xl:grid-cols-1 max-lg:grid-cols-1 max-md:grid-cols-1">
+                <div className=" box-content flex h-6 w-11/12 justify-between rounded border-none bg-white p-4 shadow-md max-sm:h-9">
+                    <p>Apakah seorang pemula bisa ikut belajar?</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
                 </div>
-                <div className="grid grid-cols-1 gap-6 pt-12 md:grid-cols-2">
-                    <div className="box-content flex h-6 w-full justify-between rounded border-4 border-none bg-white p-4 shadow-md md:w-3/4">
-                        <p>Apakah seorang pemula bisa ikut belajar?</p>
-                        <img src="/images/plus.png" alt="plus" />
-                    </div>
-                    <div className="box-content flex h-6 w-full justify-between rounded border-4 border-none bg-white p-4 shadow-md md:w-3/4">
-                        <p>Kapan saya akan mendapatkan sertifikat?</p>
-                        <img src="/images/plus.png" alt="plus" />
-                    </div>
-                    <div className="box-content flex h-6 w-full justify-between rounded border-4 border-none bg-white p-4 shadow-md md:w-3/4">
-                        <p>Pengalaman belajar di Edugrow seperti apa?</p>
-                        <img src="/images/plus.png" alt="plus" />
-                    </div>
-                    <div className="box-content flex h-6 w-full justify-between rounded border-4 border-none bg-white p-4 shadow-md md:w-3/4">
-                        <p>Apakah tersedia komunitas untuk belajar?</p>
-                        <img src="/images/plus.png" alt="plus" />
-                    </div>
-                    <div className="box-content flex h-6 w-full justify-between rounded border-4 border-none bg-white p-4 shadow-md md:w-3/4">
-                        <p>Materi yang tersedia di EduGrow ada apa saja?</p>
-                        <img src="/images/plus.png" alt="plus" />
-                    </div>
-                    <div className="box-content flex h-6 w-full justify-between rounded border-4 border-none bg-white p-4 shadow-md md:w-3/4">
-                        <p>Privilege lain apa yang bisa saya dapatkan?</p>
-                        <img src="/images/plus.png" alt="plus" />
-                    </div>
+                <div className=" box-content flex h-6 w-11/12 justify-between rounded border-none bg-white p-4 shadow-md max-sm:h-9">
+                    <p>Kapan saya akan mendapatkan sertifikat?</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
                 </div>
-            </section>
-        </Container>
+                <div className="  box-content flex h-6 w-11/12 justify-between rounded border-none bg-white p-4 shadow-md max-sm:h-9">
+                    <p>Pengalaman belajar di Edugrow seperti apa?</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
+                </div>
+                <div className="  box-content flex h-6 w-11/12 justify-between rounded border-none bg-white p-4 shadow-md max-sm:h-9">
+                    <p>Apakah tersedia komunitas untuk belajar?</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
+                </div>
+                <div className="  box-content flex h-6 w-11/12 justify-between rounded border-none bg-white p-4 shadow-md max-sm:h-9">
+                    <p>Apakah tersedia komunitas untuk belajar?</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
+                </div>
+                <div className="  box-content flex h-6 w-11/12 justify-between rounded border-none bg-white p-4 shadow-md max-sm:h-9">
+                    <p>Privilege lain apa yang bisa saya dapatkan?</p>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="h-6 w-6"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M12 4.5v15m7.5-7.5h-15"
+                        />
+                    </svg>
+                </div>
+            </div>
+        </section>
     );
 }
--- src/pages/KelasPage/KelasDetail.jsx
+++ src/pages/KelasPage/KelasDetail.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container } from "../../components/core/Grid";
+import { Container, Row, Col } from "../../components/core/Grid";
 import "./style.css";
+import { Button, Card } from "flowbite-react";
 import { Link } from "react-router-dom";
-import ContentCard from "../../components/Layout/CardComponent/ContentCard";
-import ToolsCard from "../../components/Layout/CardComponent/ToolsCard";
-import QuestionCard from "../../components/Layout/CardComponent/QuestionCard";
-import DescriptionList from "../../components/Layout/DetailClassSection/DescriptionList";
+import Question from "../DeskripsiKelas/Question";
+
+
+
 
 const KelasDetail = () => {
     const { slug } = useParams();
@@ -38,33 +39,61 @@ const KelasDetail = () => {
     return (
         <>
             {featured_image && (
-                <div className="class-content">
-                    <img
-                        className="detail-image mt-0 h-full w-full blur-sm"
-                        src={featured_image}
-                        alt={title}
-                    />
-                    <div className="header-section mx-10 absolute left-1/4 top-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <h1 className="text-[40px] font-semibold">{title}</h1>
-                        <p className="text-sm font-normal ">
-                            Dari yang awam bisa jadi ahli buat bikin produk
-                            digital keren
+                <div>
+                <section  style={{'--image-url': `url(${featured_image})`}} className="bg-[image:var(--image-url)] bg-cover bg-no-repeat">
+                <Container>
+                    <div className=" flex flex-col gap-6 pb-40 pt-40 max-sm:px-6">
+                        <h1 className="text-justify text-4xl font-bold text-white">
+                            {title}
+                        </h1>
+                        <p className="text-justify text-base text-white">
+                            Dari yang awam bisa jadi ahli buat bikin produk digital
+                            keren
                         </p>
-                        <DescriptionList />
-                        <p className="mb-6 text-sm font-normal">
-                            Gabung kelasnya dan nikmati serunya pengalaman
-                            belajar di EduGrow
+                        <div className="flex flex-row gap-8 text-white max-sm:grid grid-cols-2 ">
+                            <div className="flex flex-row items-center gap-3 ">
+                                <img src="/images/course.png" />
+                                <p>3 Courses</p>
+                            </div>
+                            <div className="flex flex-row items-center gap-3 ">
+                                <img src="/images/duration.png" />
+                                <p>3h 18m</p>
+                            </div>
+                            <div className="flex flex-row items-center gap-3 ">
+                                <img src="/images/video.png" />
+                                <p>18 video</p>
+                            </div>
+    
+                            <div className="flex flex-row items-center gap-3 ">
+                                <img src="/images/read.png" />
+                                <p>18 Reading Mterial</p>
+                            </div>
+                        </div>
+                        <div className="flex flex-row gap-8 text-white">
+                            <div className="flex flex-row items-center gap-3 ">
+                                <img src="/images/task.png" />
+                                <p>2 Kuis dan Latihan</p>
+                            </div>
+                            <div className="flex flex-row items-center gap-3 ">
+                                <img src="/images/sertif.png" />
+                                <p>Sertifikat</p>
+                            </div>
+                        </div>
+                        <p className="text-justify text-base text-white">
+                            Gabung kelasnya dan nikmati serunya pengalaman belajar
+                            di EduGrow
                         </p>
-                        <Link
-                            to={`./KelasOnline.jsx`}
-                            className="dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg bg-[#C8D4E4] px-5 py-2.5 text-center text-sm font-semibold text-black hover:bg-[#d1dbe7] focus:outline-none focus:ring-4 focus:ring-cyan-300"
-                        >
-                            Gabung Sekarang
-                        </Link>
+                        <div className="button">
+                            <Button className="btn-join font-semibold" size={"xl"}>
+                                Gabung Sekarang
+                            </Button>
+                        </div>
                     </div>
-                    <Container>
-                        <div className="mx-0 my-12 max-[350px]:mx-4">
-                            <div className="content-desc flex flex-col gap-4 md:flex-col lg:flex-col xl:flex-row">
+                </Container>
+            </section>
+            <Container>
+            <div className="mx-0 my-12 max-[350px]:mx-4 max-sm:px-6">
+                            <div className="content-desc flex flex-col gap-6 md:flex-col lg:flex-col xl:flex-row">
                                 <div className="left-content">
                                     <h1 className="sub-title my-10 text-2xl font-semibold">
                                         Deskripsi Course
@@ -75,74 +104,72 @@ const KelasDetail = () => {
                                     <h1 className="sub-title my-10 text-2xl font-semibold">
                                         Point Utama
                                     </h1>
-                                    <div className="point-list flex gap-4">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 24 24"
-                                            fill="currentColor"
-                                            className="h-6 w-6"
-                                        >
-                                            <path
-                                                fillRule="evenodd"
-                                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-                                                clipRule="evenodd"
-                                            />
-                                        </svg>
-                                        <p>
-                                            Lorem, ipsum dolor sit amet
-                                            consectetur adipisicing elit.
-                                            Deleniti, fugit.
-                                        </p>
-                                    </div>
+                                    <div className="flex flex-col gap-8 pb-4">
+                        <div className="flex gap-8">
+                            <img src="/images/check.png" alt="check" />
+                            <p>Mengenal tools Whimsical dan Figma</p>
+                        </div>
+                        <div className="flex gap-8">
+                            <img src="/images/check.png" alt="check" />
+                            <p>
+                                Belajar mengeai style guide, layout grid dan
+                                auto layout di Figma
+                            </p>
+                        </div>
+                        <div className="flex gap-8">
+                            <img src="/images/check.png" alt="check" />
+                            <p>Mengetahui langkah-langkah membuat UI design</p>
+                        </div>
+                        <div className="flex gap-8">
+                            <img src="/images/check.png" alt="check" />
+                            <p>Praktek membuat UI design Website</p>
+                        </div>
+                    </div>
                                 </div>
-                                <div className="right-content">
-                                    <h1 className="sub-title my-10 text-2xl font-semibold">
-                                        Konten Kelas
-                                    </h1>
-                                    <ContentCard />
+                                <div className="right-content "> 
                                     <h1 className="sub-title my-10 text-2xl font-semibold">
                                         Tools Pendukung
                                     </h1>
-                                    <ToolsCard />
-                                    <div className="tools"></div>
+                                    <div className="flex flex-col gap-4 md:flex-row max-sm:pl-12">
+                        <Card className="w-[255px] h-[348px] ">
+                            <img className="h-[93px] w-[93px] object-left-top" src="/images/Figma-logo.png" alt="Figma" />
+                            <h1 className="dark:text-gray-400 text-2xl font-semibold text-gray-700">
+                                Figma
+                            </h1>
+                            <p className="text-base text-gray-700">
+                                Software Gratis
+                            </p>
+                            <Link
+                                to='https://www.figma.com/downloads/'
+                                className="mt-6 w-[200px]
+                                h-[69px] rounded-md text-[#3A5088] py-6 text-center text-sm font-semibold bg-white outline focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                            >
+                                Download Now
+                            </Link>
+                        </Card>
+                        <Card className="w-[255px] h-[348px] ">
+                            <img className="h-[93px] w-[93px] object-left-top" src="/images/Whimsical.png" alt="Figma" />
+                            <h1 className="dark:text-gray-400 text-2xl font-semibold text-gray-700">
+                                Whimsical
+                            </h1>
+                            <p className="text-base text-gray-700">
+                                Software Gratis
+                            </p>
+                            <Link
+                                to='https://www.figma.com/downloads/'
+                                className="mt-6 w-[200px]
+                                h-[69px] rounded-md text-[#3A5088] py-6 text-center text-sm font-semibold bg-white outline focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+                            >
+                                Download Now
+                            </Link>
+                        </Card>
+                    </div>
                                 </div>
                             </div>
-                            <div className="quetion mt-20">
-                                <p className="text-center font-semibold text-[#3A5088]">
-                                    Mari Bertanya!
-                                </p>
-                                <h1 className="mt-4 text-center text-4xl font-black">
-                                    Frequently{" "}
-                                </h1>
-                                <h1 className="mb-6 mt-2 text-center text-4xl font-extrabold">
-                                    Asked Question
-                                </h1>
-                                <QuestionCard />
+                            <Question />
                             </div>
+        </Container>
 
-                            {/* {setKelas.map((e)=>(
-               <div key={e.slug}>
-
-                   <p>{e.summary}</p>
-               </div> 
-            ))} */}
-                            {/* <div className="mb-4 flex items-center">
-                <img
-                    className="mr-2 h-10 w-10 rounded-full"
-                    src={author.profile_image}
-                    alt={`${author.first_name} ${author.last_name}`}
-                />
-                <div>
-                    <p className="text-sm text-gray-600">
-                        {author.first_name} {author.last_name}
-                    </p>
-                    <p className="text-sm text-gray-600">{created}</p>
-                </div>
-            </div> */}
-
-                            {/* <div className="mb-20" dangerouslySetInnerHTML={{ __html: body }} /> */}
-                        </div>
-                    </Container>
                 </div>
             )}
         </>
@@ -150,3 +177,55 @@ const KelasDetail = () => {
 };
 
 export default KelasDetail;
+
+            //     <div className="class-content">
+            //         <img
+            //             className="detail-image mt-0 h-full w-full blur-sm"
+            //             src={featured_image}
+            //             alt={title}
+            //         />
+            //         <div className="header-section mx-10 absolute left-1/4 top-1/2 -translate-x-1/2 -translate-y-1/2">
+            //             <h1 className="text-[40px] font-semibold">{title}</h1>
+            //             <p className="text-sm font-normal ">
+            //                 Dari yang awam bisa jadi ahli buat bikin produk
+            //                 digital keren
+            //             </p>
+            //             <DescriptionList />
+            //             <p className="mb-6 text-sm font-normal">
+            //                 Gabung kelasnya dan nikmati serunya pengalaman
+            //                 belajar di EduGrow
+            //             </p>
+            //             <Link
+            //                 to={`./KelasOnline.jsx`}
+            //                 className="dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg bg-[#C8D4E4] px-5 py-2.5 text-center text-sm font-semibold text-black hover:bg-[#d1dbe7] focus:outline-none focus:ring-4 focus:ring-cyan-300"
+            //             >
+            //                 Gabung Sekarang
+            //             </Link>
+            //         </div>
+                    //<Container>
+                        
+
+            //                 {/* {setKelas.map((e)=>(
+            //    <div key={e.slug}>
+
+            //        <p>{e.summary}</p>
+            //    </div> 
+            // ))} */}
+            //                 {/* <div className="mb-4 flex items-center">
+            //     <img
+            //         className="mr-2 h-10 w-10 rounded-full"
+            //         src={author.profile_image}
+            //         alt={`${author.first_name} ${author.last_name}`}
+            //     />
+            //     <div>
+            //         <p className="text-sm text-gray-600">
+            //             {author.first_name} {author.last_name}
+            //         </p>
+            //         <p className="text-sm text-gray-600">{created}</p>
+            //     </div>
+            // </div> */}
+
+            //                 {/* <div className="mb-20" dangerouslySetInnerHTML={{ __html: body }} /> */}
+            //             </div>
+            //         </Container>
+            //     </div>
